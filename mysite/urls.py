33fc3b576f97a6ae9eddbac69877aab3f2e596a4
Wei Xu <ginues109@qq.com>
2015-03-31 17:58:51 +0800

    views_learning

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^polls/', include('polls.urls')),
     url(r'^admin/', include(admin.site.urls)),
     (r'^hello/$', hello),
     (r'^time/$', current_datetime)
