b18daf561882892d1c1da77339ea3a1a66bbe632
Wei Xu <ginues109@qq.com>
2015-04-01 17:42:08 +0800

    intern start
    migration

---
+++ intern/migrations/0001_initial.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Author',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=40)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Blog',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=60)),
+                ('content', models.TextField()),
+                ('publish_time', models.DateTimeField(auto_now_add=True)),
+                ('author', models.ForeignKey(to='intern.Author')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
