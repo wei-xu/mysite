97e49e17288b31e396f7f89eb2878042bca66553
ginues109 <ginues109@qq.com>
2015-04-07 11:27:21 +0800

    Change from sqlite to postgresql

--- mysite/settings.py
+++ mysite/settings.py
@@ -62,8 +62,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': 'postgres',
+        'USER':'postgres',
+        'PASSWORD': 'yuxin09211',
+        'HOST':'',
+        'PORT':'5432',
     }
 }
 
